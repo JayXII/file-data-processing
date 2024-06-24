44bef614eec584c4af9b392c432f705c4da9f874
John Mootsi <jmootsi021@student.wethinkcode.co.za>
2024-06-24 06:50:39 +0200

    Added EnvironmetalData entity

---
+++ src/main/java/com/enviro/assessment/grad001/johnmootsi/entities/EnvironmentalDataEntity.java
@@ -0,0 +1,24 @@
+package com.enviro.assessment.grad001.johnmootsi.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "ENVIRONMENTAL DATA")
+@Getter
+@Setter
+@NoArgsConstructor
+public class EnvironmentalDataEntity {
+    @Id()
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    private Long id;
+    @Column(name = "NAME", nullable = false, length = 50)
+    private String environmentName;
+    @Column(name = "DESCRIPTION", nullable = false, length = 500)
+    private String environmentDescription;
+    @Column(name = "TYPE", nullable = false, length = 50)
+    private String environmentType;
+}
