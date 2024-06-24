77db22edefaddcdf5812a922eabcd5bcdbd2b8bf
John Mootsi <jmootsi021@student.wethinkcode.co.za>
2024-06-24 06:16:16 +0200

    Added endpoints

--- src/main/java/com/enviro/assessment/grad001/johnmootsi/controllers/FileProcessorController.java
+++ src/main/java/com/enviro/assessment/grad001/johnmootsi/controllers/FileProcessorController.java
@@ -1,8 +1,9 @@
 package com.enviro.assessment.grad001.johnmootsi.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 
 @RestController
 @RequestMapping("/api/v1")
@@ -12,5 +13,23 @@ public class FileProcessorController {
     @GetMapping("/hello")
     public String helloWorld() {
         return "Hello, world!";
+    }
+
+    // File upload endpoint
+    @PostMapping("/upload")
+    public ResponseEntity<String> processFile(@RequestParam("file") MultipartFile file) {
+        return ResponseEntity.ok("Hello, ");
+    }
+
+    // Get results with id endpoint
+    @GetMapping("/results/{id}")
+    public ResponseEntity<String> getProcessedResultsById(@PathVariable Long id) {
+        return ResponseEntity.ok("Hello, ");
+    }
+
+    // Get all results
+    @GetMapping("/results")
+    public ResponseEntity<String> getAllProcessedResults() {
+        return ResponseEntity.ok("Hello, ");
     }
 }
